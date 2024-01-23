c27dec4ae0a0ba499ca743992c0110a0df297682
Ryster <ry.diffusion@proton.me>
2024-01-23 14:16:41 -0300

    feat: I HAVE FRONTEND

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+	en: "English",
+	br: "Brasileiro",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+	en: {
+		"nav.home": "Home",
+		"nav.about": "About me",
+	},
+	br: {
+		"nav.home": "Home",
+		"nav.about": "Sobre mim",
+	},
+} as const;
